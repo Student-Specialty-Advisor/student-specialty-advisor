2e5496a1afdcce99f2fd1c963181fd4569c5001f
unknown <hammami.aym@outlook.com>
2022-02-08 21:32:28 +0100

    Frontend: Added signup and signin forms

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import SignInForm from "./components/SignInForm";
 
 function App() {
   return (
-    /*<h1> PUT SOMETHING TO RENDER IN APP.JS </h1>*/
-    <SignInForm/>
+    <h1> PUT SOMETHING TO RENDER IN APP.JS </h1>
   );
 }
 
