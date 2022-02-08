cc1b096699b6d91c42b2f3b310f184a32f4596bd
aziz-maazouz <aziz.maazouz@medtech.tn>
2022-02-08 21:38:06 +0100

    Backend: Added accountSystemController.js, created Api endpoints and initialized controller

--- server/index.js
+++ server/index.js
@@ -3,11 +3,26 @@ const db = require("./db");
 /*const path = require("path");*/
 const app = express();
 const PORT = process.env.PORT || 8000;
+const controller = require("./resources/accountSystemController.js")
 
 /*const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath)); LEAVE THIS COMMENTED*/
 
 /* use app.put / app.get ... here without a router class. It's necessary for heroku deployment */
+
+app.use(express.json());
+
+app.post("/as-api/sign-up", controller.SignUp);
+app.post("/as-api/log-in", controller.LogIn);
+/*app.get("/as-api/", );
+app.delete("/as-api/", );*/
+app.put("/as-api/:username/", controller.EditAccount);
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Started listening to requests on port ${PORT}`);
---
+++ server/resources/accountSystemController.js
@@ -0,0 +1,16 @@
+var SignUp = (req, res) => {   //Sign up
+
+};
+
+var LogIn = (req, res) => { //Sign in
+  
+};
+
+var EditAccount = (req, res) => { // Edit Account info
+  
+};
+
+// Exports
+exports.SignUp = SignUp;
+exports.LogIn = LogIn;
+exports.EditAccount = EditAccount;
