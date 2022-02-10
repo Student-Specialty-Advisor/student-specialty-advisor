ec81b587c3ed5e9f404ee263cb7cd22d1b7998a3
unknown <hammami.aym@outlook.com>
2022-02-10 19:50:52 +0100

    Backend: Login controller

--- server/controllers/accountSystemController.js
+++ server/controllers/accountSystemController.js
@@ -1,9 +1,12 @@
 const User = require("../db/User");
 
+const invalidPassword = { keyPattern: { password: 1 } };
+const invalidEmail = { keyPattern: { email: 1 } };
+
 var SignUp = (req, res) => {
   // Endpoint: /as-api/sign-up
   const json = req.body;
-  var hashedPassword = "hash"; // Bcrypt here
+  var hashedPassword = "hash"; // Hash json.password
   var userData = {
     firstName: json.firstName,
     lastName: json.lastName,
@@ -24,7 +27,19 @@ var SignUp = (req, res) => {
 };
 
 var LogIn = (req, res) => {
-  //Sign in
+  //Endpoint: /as-api/log-in
+  const json = req.body;
+  var emailObj = { email: json.email };
+  User.findOne(emailObj).then((user) => {
+    if (user === null) {
+      res.status(500).send(invalidEmail);
+    } else {
+      var hashedPassword = "hash"; // Hash json.password
+      // Compare hashedPassword with user.hash
+      // If true, res.status(200).send(user)
+      // If false, res.status(500).send(invalidPassword)
+    }
+  });
 };
 
 var EditAccount = (req, res) => {
