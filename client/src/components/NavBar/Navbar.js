f38dd23474aa9580bcb3f42c859b67f74c91b520
mahdi klouz <mahdiklouz@mahdis-MacBook-Air.local>
2022-02-10 16:02:30 +0100

    Front: added Logo

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -1,49 +1,49 @@
-import React from 'react';
+import React from "react";
+import SmuLogoTree from "../../assets/Images/SmuLogoTree.png";
 import {
-Nav,
-NavLink,
-Bars,
-NavMenu,
-NavBtn,
-NavBtnLink,
-} from './NavbarElements';
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 function Navbar() {
-return (
-	<>
-	<Nav>
-		<Bars />
+  return (
+    <>
+      <Nav>
+        <Bars />
 
-		<NavMenu>
-		<NavLink to='/' activeStyle>
-			"Logo"
-		</NavLink>
-		<NavLink to='/about' activeStyle>
-			About
-		</NavLink>
-		<NavLink to='/videos' activeStyle>
-			Videos
-		</NavLink>
-		<NavLink to='/curricilum' activeStyle>
-			Curricilum
-		</NavLink>
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-up'>Sign Up</NavBtnLink> */}
-		</NavMenu>
-		<NavBtn>
-		<NavBtn>
-		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
-		</NavBtn>
-		<NavBtn>
-		<NavBtnLink to='/signup'>Sign Up</NavBtnLink>
-		</NavBtn>
-		</NavBtn>
-	</Nav>
-	</>
-);
-};
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            <img src={SmuLogoTree} alt="not working"></img>
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/videos" activeStyle>
+            Videos
+          </NavLink>
+          <NavLink to="/curricilum" activeStyle>
+            Curricilum
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-up'>Sign Up</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+          <NavBtn>
+            <NavBtnLink to="/signup">Sign Up</NavBtnLink>
+          </NavBtn>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+}
 
 export default Navbar;
-
