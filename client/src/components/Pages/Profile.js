f2452ed52a2910625430b1a2cc75082a632478a6
youssef-aydi <77480931+youssef-aydi@users.noreply.github.com>
2022-02-12 17:30:20 +0100

    Front-end : edit profile page with the correct user info

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,30 +1,31 @@
 import React from "react";
-
+import Data from "../../services/AuthService";
 function Profile() {
+  const userData = Data.getCurrentUser();
   return (
     <div>
       <h1>Welcome to Your Profile!</h1>
       <br />
       <h4>
-        <b>First Name :</b>
-        test
+        <b>First Name : </b>
+        {userData.firstName}
       </h4>
       <br />
       <h4>
-        <b>Last Name :</b>
-        test
+        <b>Last Name : </b>
+        {userData.lastName}
       </h4>
       <br />
       <h4>
-        <b>Email :</b>
-        test
+        <b>Email : </b>
+        {userData.email}
       </h4>
       <br />
-      <h4>
-        <b>Password :</b>
-        test
+      {/*<h4>
+        <b>Password : </b>
+        {userData.password}
       </h4>
-      <br />
+      <br />*/}
     </div>
   );
 }
