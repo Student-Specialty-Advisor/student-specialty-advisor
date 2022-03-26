72248c909b1a4471146b119a114acfb454fc5fdd
youssef-aydi <77480931+youssef-aydi@users.noreply.github.com>
2022-03-26 10:17:50 +0100

    meetingContoller created to get list of advisors

---
+++ server/controllers/meetingController.js
@@ -0,0 +1,15 @@
+try {
+  var advisors = require("../resources/advisor.json");
+} catch (error) {
+  var advisors = null;
+}
+
+var getListOfAdvisors = (req, res) => {
+  if (advisors === null) {
+    res.status(500).send({ error: "advisors.json file was not found." });
+  } else {
+    res.status(200).send(advisors);
+  }
+};
+
+exports.getListOfAdvisors = getListOfAdvisors;
