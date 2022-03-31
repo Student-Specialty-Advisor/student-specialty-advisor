b671cfa3a7ff9981783f39972e856e615f85e899
Mahdi Klouz <mahdi.klouz@medtech.tn>
2022-03-31 01:41:12 +0100

    Front: unfinished about page

--- client/src/components/Pages/Meeting Page/MeetingsAbout.js
+++ client/src/components/Pages/Meeting Page/MeetingsAbout.js
@@ -1,25 +1,61 @@
 import React from "react";
+import Footer from "../Footer";
 
-function MeetingsAbout() {
+function MeetingsAbout(props) {
   return (
     <>
-      <div className="about-container">
-        <div className="about-container-test">
-          <h1>What are the meetings that you can book</h1>
-        </div>
-        <h1>What are the meetings used for</h1>
-        <h3> The use of this feature </h3>
-        <li>talk to professors about the specialty</li>
-        <li>talk to students who have experienced the specialty</li>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui
-          numquam unde ipsum, atque ex adipisci dolorum et laborum vero
-          cupiditate architecto, totam cum repellendus similique assumenda
-          perferendis. Necessitatibus ipsum vero animi at tempore obcaecati
-          repudiandae architecto cupiditate voluptatum libero neque quo,
-          laboriosam velit. Soluta repudiandae quaerat ipsa quae possimus?
-        </p>
+      <div className="about-container-test">
+        <h1>What are the meetings that you can book</h1>
       </div>
+      <div className="about-container">
+        <h1>What are Meetings?</h1>
+        <h4>
+          In this page students will find the description of this feature and
+          how to use it
+        </h4>
+        <h3> The use of this feature </h3>
+        <li>talk to professors about the programs</li>
+        <li>schedule a Meeting with a PASS leader </li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <div className="about-container-img"></div>
+      </div>
+      <div className="about-container-mid">
+        <h3> When to schedule a meeting</h3>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <h3> What is the outcome of using this feature?</h3>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+
+        <h3>Students may find the list of the advisors here </h3>
+        <button
+          className="about-container-mid-button"
+          onClick={() => {
+            props.history.push("/meetings/advisors");
+          }}
+        >
+          Advisors List
+        </button>
+      </div>
+      <div className="about-container-bottom">
+        <h3>Students may requset a meeting here </h3>
+        <button
+          className="about-container-bottom-button"
+          onClick={() => {
+            props.history.push("/meetings/request");
+          }}
+        >
+          REQUEST A MEETING!
+        </button>
+      </div>
+      <Footer />
     </>
   );
 }
