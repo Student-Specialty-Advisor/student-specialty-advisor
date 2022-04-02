1352ba387c2a5e078dd1539aa8148369500ed49f
youssef-aydi <77480931+youssef-aydi@users.noreply.github.com>
2022-04-02 13:45:37 +0100

    backend : advisor model created

--- server/db/Advisor.js
+++ server/db/Advisor.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
-const Advisor = mongoose.model("Advisor", {});
+const Advisor = mongoose.model("Advisor", {
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  profession: String,
+  linkedinUrl: { type: String, unique: true },
+  imageUrl: { type: String, unique: true },
+});
 
 module.exports = Advisor;
