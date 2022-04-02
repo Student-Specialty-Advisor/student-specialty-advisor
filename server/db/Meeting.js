250cba8c6f6559624cf0c453ce99cc4acbe19332
youssef-aydi <77480931+youssef-aydi@users.noreply.github.com>
2022-04-02 21:50:13 +0100

    updated meeting .js

--- server/db/Meeting.js
+++ server/db/Meeting.js
@@ -5,7 +5,7 @@ const MeetingSchema = new Schema({
   day: String,
   from: String,
   to: String,
-  advisorName: { type: Schema.Types.ObjectId, ref: "Advisor" },
+  advisorName: String,
 });
 MeetingSchema.index(
   { day: 1, from: 1, to: 1, advisorName: 1 },
