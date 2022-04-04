613c7569408a98171f43411124ae2dd5cf2a085f
aymendps <hammami.aym@outlook.com>
2022-04-04 18:24:57 +0100

    Updated meetingController.js

--- server/controllers/meetingController.js
+++ server/controllers/meetingController.js
@@ -50,8 +50,8 @@ var requestMeeting = (req, res) => {
       " requests a meeting with you!",
     text: "",
     html:
-      "<div style='padding:5px; background-color: #10A5F5; border-radius: 25px;'>" +
-      "<div style='text-align: center; background-color: rgb(32, 53, 73); border-radius:25px;' >" +
+      "<div style='padding:5px; background-color: #10A5F5;'>" +
+      "<div style='text-align: center; background-color: rgb(32, 53, 73);' >" +
       "<br/><p style='text-align:left; margin-left:5%; margin-right:5%; color:white;'><span style='font-style: bold'>Dear Advisor,</span><br/><br/>We wanted to let you know that member <strong>" +
       data.userName +
       "</strong> has requested a meeting with you on <strong>" +
@@ -73,7 +73,7 @@ var requestMeeting = (req, res) => {
       "<br/><br/>As always, we are extremely grateful for your effort as an advisor to the South Mediterranean University community and we thank you for your time and understanding." +
       "<br/><br/><br/><i>By Students, For Students,<br/>The Student Specialty Advisor Team</i>" +
       "</p><br/>" +
-      "<a href='https://student-specialty-advisor.herokuapp.com/' target='_blank' rel='noreferrer noopener'><div style='width:100%; border-bottom-left-radius: 25px; border-bottom-right-radius: 25px; background-color: rgb(15, 25,33); padding-top: 2%; padding-bottom: 2%;'><img style='width:25%;' src= https://i.imgur.com/9oEMMqC.png alt=''/></div></a>" +
+      "<a href='https://student-specialty-advisor.herokuapp.com/' target='_blank' rel='noreferrer noopener'><div style='width:100%; background-color: rgb(15, 25,33); padding-top: 2%; padding-bottom: 2%;'><img style='width:25%;' src= https://i.imgur.com/9oEMMqC.png alt=''/></div></a>" +
       "</div></div>",
   };
   sendEmail(data.to, email.subject, email.text, email.html, res);
