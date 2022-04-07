f205b672eb0e61459f6d5539b35ff490f39ee7e2
aziz-maazouz <aziz.maazouz@medtech.tn>
2022-04-07 22:56:12 +0100

    FRONTEND: Added minimal UI to meetings/advisors

--- client/src/components/Pages/Meeting Page/MeetingsAdvisorsList.js
+++ client/src/components/Pages/Meeting Page/MeetingsAdvisorsList.js
@@ -25,12 +25,38 @@ function MeetingsAdvisorsList() {
   });
 
   return (
-    <>
-      <div className="meetings-advisors-list">
-        <ul>{advisorList}</ul>
+    <div className="advisors-page-container">
+      <div className="advisors-about-container">
+        <h2> What does an advisor do ?</h2>
+        <p>
+          Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+          Blah Blah Blah Blah Blah Blah Blah Blah
+        </p>
+        <h2> Can I be an advisor ?</h2>
+        <p>
+          Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+          Blah Blah Blah Blah Blah Blah Blah Blah
+        </p>
       </div>
-    </>
+      <button>Apply now!</button>
+    </div>
   );
 }
 
 export default MeetingsAdvisorsList;
+/*
+<p style={{ fontSize: "16px", position: "absolute" }}>
+        TEMPORARY PAGE WITH ADVISOR CARD COMPONENT
+      </p>
+      <div className="meetings-advisors-list">
+        <ul>
+          <AdvisorCard
+            fullname="Advisor's Name"
+            picture={testPicture}
+            profession="Senior Student"
+            quote="'Quote or smthg qsdq sdq q sd q d qsd qs d qd s'"
+            email="https://www.outlook.com"
+            linkedin="https://www.linkedin.com/"
+          />
+        </ul>
+      </div> */
