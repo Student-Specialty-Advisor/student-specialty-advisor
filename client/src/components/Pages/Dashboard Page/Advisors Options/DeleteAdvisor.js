132fd9ef075b5981825826899d88d0fea94dd385
youssef-aydi <77480931+youssef-aydi@users.noreply.github.com>
2022-04-10 17:01:41 +0100

    frontend dashboard delete advisor option

---
+++ client/src/components/Pages/Dashboard Page/Advisors Options/DeleteAdvisor.js
@@ -0,0 +1,38 @@
+import fetchService from "../../../../services/fetchService.js";
+import alertify from "alertifyjs";
+import React, { useRef } from "react";
+
+function DeleteAdvisor(props) {
+  const select = useRef();
+  const task = () => {
+    fetchService
+      .doDelete("meeting/advisors/" + select.current.value)
+      .then((response) => {
+        if (response.success) {
+          alertify.success("Advisor was deleted Successfully");
+          props.setAdvisorsList();
+        } else {
+          alertify.error(
+            "Please try later , an error has occurred while deleting the advisor"
+          );
+        }
+      })
+      .catch(() => {
+        alertify.error(
+          "Please try later , an error has occurred while deleting the advisor"
+        );
+      });
+  };
+  const mapping = props.advisorsList.map((advisor) => {
+    return <option value={advisor._id}>{advisor.fullName}</option>;
+  });
+  return (
+    <>
+      <h1>Delete Advisor :</h1>
+      <select ref={select}>{mapping}</select>
+      <button onClick={task}>Submit</button>
+    </>
+  );
+}
+
+export default DeleteAdvisor;
--- client/src/components/Pages/Dashboard Page/AdvisorsOptions.js
+++ client/src/components/Pages/Dashboard Page/AdvisorsOptions.js
@@ -1,14 +1,18 @@
 import React from "react";
 import AddAdvisor from "./Advisors Options/AddAdvisor";
+import DeleteAdvisor from "./Advisors Options/DeleteAdvisor";
 
 function AdvisorsOptions(props) {
   return (
     <ul className="dashboard-options-list">
       <li>
-        <AddAdvisor />
+        <AddAdvisor setAdvisorsList={props.setAdvisorsList} />
       </li>
       <li>
-        <p>Your component goes here inside the li</p>
+        <DeleteAdvisor
+          setAdvisorsList={props.setAdvisorsList}
+          advisorsList={props.advisorsList}
+        />
       </li>
       <li>
         <p>Your component goes here inside the li</p>
