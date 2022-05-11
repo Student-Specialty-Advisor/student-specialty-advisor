d472339dd7ada9bada3e17a815577466604abc89
aymendps <hammami.aym@outlook.com>
2022-05-11 21:17:03 +0100

    2nd try

--- server/index.js
+++ server/index.js
@@ -253,7 +253,7 @@ app.put(
 //#endregion
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
 });
 
 app.listen(PORT, () => {
